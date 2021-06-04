49240a84b6563fe8844161f6aaffa3cde8e44d2e
windy <kugoo11v12@126.com>
2021-06-04 15:41:47 +0800

    修正 WaitForSeconds

--- assets/scripts/PathFinding/Runtime/Basic/WaitForSeconds.ts
+++ assets/scripts/PathFinding/Runtime/Basic/WaitForSeconds.ts
@@ -3,6 +3,6 @@ export function WaitForSeconds(secs: number): Promise<void> {
 	return new Promise<void>((resolve, reject) => {
 		setTimeout(() => {
 			resolve()
-		}, secs / 1000)
+		}, secs * 1000)
 	})
 }
